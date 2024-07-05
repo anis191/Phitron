9c40a3662614e24c31d3669d7e14fbd890a54a8b
anisul alam <anisulalam2002@gmail.com>
2024-07-05 16:11:39 +0600

    resolve it

--- Second Semester/Basic Data Structures/Week-02/Module-08(Assignment-02)/Remove_Duplicate.cpp
+++ Second Semester/Basic Data Structures/Week-02/Module-08(Assignment-02)/Remove_Duplicate.cpp
@@ -1,3 +1,70 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Node{
+// public:
+//     int val;
+//     Node *next;
+//     Node(int val){
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+// void insert_at_tail(Node* &head, Node* &tail, int v){
+//     Node *newNode = new Node(v);
+//     if(head == NULL){
+//         head = newNode;
+//         tail = newNode;
+//         return;
+//     }
+//     tail->next = newNode;
+//     tail = newNode;
+//     return;
+// }
+// void sort_linked_list(Node *head){
+//     for(Node *i=head; i->next != NULL; i=i->next){
+//         for(Node *j=i->next; j!=NULL; j=j->next){
+//             if(i->val > j->val){
+//                 swap(i->val,j->val);
+//             }
+//         }
+//     }
+// }
+// void linked_list_recursion(Node *n){
+//     if(n == NULL ||  n->next == NULL){
+//         return;
+//     }
+//     if(n->val == n->next->val){
+//         Node *deleteNode = n->next;
+//         n->next = n->next->next;
+//         delete deleteNode;
+//         linked_list_recursion(n);
+//     }else{
+//         linked_list_recursion(n->next);
+//     }
+// }
+// void print_linked_list(Node *head){
+//     Node *temp = head;
+//     while(temp != NULL){
+//         cout<<temp->val<<" ";
+//         temp = temp->next;
+//     }
+//     cout<<endl;
+//     return;
+// }
+// int main(){
+//     Node *head = NULL;
+//     Node *tail = NULL;
+//     while(true){
+//         int value; cin>>value;
+//         if(value == -1){
+//             break;
+//         }
+//         insert_at_tail(head,tail,value);
+//     }
+//     sort_linked_list(head);
+//     linked_list_recursion(head);
+//     print_linked_list(head);
+// }
 #include <bits/stdc++.h>
 using namespace std;
 class Node{
@@ -20,62 +87,50 @@ void insert_at_tail(Node* &head, Node* &tail, int v){
     tail = newNode;
     return;
 }
-int size_of_linked_list(Node *head){
-    int count = 0;
-    Node *temp = head;
-    while(temp != NULL){
-        count++;
-        temp = temp->next;
-    }
-    return count;
-}
-void duplicate_found(Node *&head, Node* &tail, int pos){
-    if(pos == 1){
-        Node *deleteNode = head;
-        head = head->next;
-        delete deleteNode;
-        if(head == NULL){
-            tail = NULL;
-        }
-    }else{
-        Node *tmp = head;
-        for(int i=1; i<pos-1; i++){
-            tmp = tmp->next;
-        }
-        Node *deleteNode = tmp->next;
-        tmp->next = tmp->next->next;
-        if (deleteNode == tail) {
-            tail = tmp;
-        }
-        delete deleteNode;
-    }
-}
-void delete_duplicate(Node *head, Node* &tail){
-    int pos1 = 0;
-    for(Node *i=head; i != NULL; i=i->next){
-        pos1++;
-        Node *p = i;
-        int pos2 = pos1;
-        for (Node *j=i->next; j!=NULL; j=j->next){
-            pos2++;
-            if(i->val == j->val){
-                duplicate_found(head,tail,pos2);
-                pos2--;
-            }else {
-                p=j;
+void sort_linked_list(Node *head){
+    for(Node *i=head; i->next != NULL; i=i->next){
+        for(Node *j=i->next; j!=NULL; j=j->next){
+            if(i->val > j->val){
+                swap(i->val,j->val);
             }
         }
     }
 }
+// void linked_list_recursion(Node* &tail,Node *n){
+//     int c = 0;
+//     if(n == NULL){
+//         return;
+//     }
+//     linked_list_recursion(tail,n->next);
+//     // cout<<n->val<<" ";
+//     Node *tmp = n;
+//     if(tmp->val == tmp->next->val){
+//         tmp->next = tmp->next->next;
+//     }
+// }
+void linked_list_recursion(Node *n) {
+    if (n == NULL || n->next == NULL) {
+        return;
+    }
+    if (n->val == n->next->val) {
+        Node *temp = n->next;
+        n->next = temp->next;
+        delete temp;
+        linked_list_recursion(n); // Check again after deletion
+    } else {
+        linked_list_recursion(n->next); // Move to next node
+    }
+}
+
 void print_linked_list(Node *head){
     Node *temp = head;
-    while(temp != NULL){
+    while(temp != NULL){    //n-times --> O(n)
         cout<<temp->val<<" ";
         temp = temp->next;
     }
+    cout<<endl;
     return;
 }
-
 int main(){
     Node *head = NULL;
     Node *tail = NULL;
@@ -86,7 +141,7 @@ int main(){
         }
         insert_at_tail(head,tail,value);
     }
-    delete_duplicate(head,tail);
+    sort_linked_list(head);
+    linked_list_recursion(head);
     print_linked_list(head);
-    return 0;
 }
